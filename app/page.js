e21f092df6229572123d52ceb07f9bc92799defc
Aram Petrosyan <142950097+aarampetrosyann@users.noreply.github.com>
2024-08-08 20:16:51 -0700

    Updated Helper Functs
    Updated removeItem and added addItem

--- app/page.js
+++ app/page.js
@@ -24,7 +24,22 @@ export default function Home() {
     console.log(inventoryList)
   }
 
-  const removeItem = async (item => {
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      await setDoc(docRef, {quantity: quantity + 1})
+    }
+    else{
+      await setDoc(docRef, {quantity: 1})
+    }
+
+    await updateInventory()
+  }
+
+  const removeItem = async (item) => {
     const docRef = doc(collection(firestore, 'inventory'), item)
     const docSnap = await getDoc(docRef)
 
@@ -34,10 +49,12 @@ export default function Home() {
         await deleteDoc(docRef)
       }
       else{
-        
+        await setDoc(docRef, {quantity: quantity - 1})
       }
     }
-  })
+
+    await updateInventory()
+  }
 
   useEffect(()=>{
     updateInventory()
